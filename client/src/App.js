e8ccd5b09bdd897b4f9a8a2fd7e6955a0dc30c7d
ericl <npgolfleague@gmail.com>
2024-03-06 16:21:04 -0500

    first commit with user test version

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
+        <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+                <form action="">
+                    <div className="mb-3">
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder="Enter Email" className="form-control rounded-0" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder="Enter Password" className="form-control rounded-0" />
+                    </div>
+                    <button className="btn btn-success w-100 rounded-0"><strong>Login</strong></button>
+                    <p>By logging in you are agreeing to our terms and policies</p>
+                    <Link to="/signup" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"><strong>Create Account</strong></Link>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
