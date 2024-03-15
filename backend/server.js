a89876d71f84319c403736bfc0c4f5d188563827
Eric Lindquist <npgolfleague@gmail.com>
2024-03-15 14:54:11 -0400

    more user changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,37 @@
-const express = require("express");
+const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 
-const db = mysql.createConnection(
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    
-)
+    password: "Zin!1Ter9",
+    database: "golf"
+})
+
+app.post('/signup', (req, res) => {
+    const sql = "INSERT INTO users  ('name' , 'email', 'phone','quota', 'password') VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.phone,
+        req.body.quota,
+        req.body.password
+    ]
+    console.log(values);
+    db.query(sql, [values], (err, data) => {
+        if (err) {
+            console.log("error path");
+            return res.json("Error");
+        }
+        console.log("data path");
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("Listening on port 8081");
+})
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import Validation from './LoginValidation';
 
 function Signup() {
     const [values, setValues] = useState({
+        name: '',
         email: '',
+        phone: '',
+        quota: '',
         password: ''
     })
     const [errors, setErrors] = useState({})
     const handleInput =(event) => {
+        console.log("handleInput entry");
         setValues(prev => ({ ...prev, [event.target.name]: [event.target.value]}))
 
     }
     const handleSubmit =(event) => {
+        console.log("handleSubmit entry");
         event.preventDefault();
         setErrors(Validation(values));
+        console.log(errors); 
+        if( errors.email ==="" && errors.password ==="") {
+            console.log("building post");
+            axios.post('http://localhost:8081/signup', values)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        }
     }
     return (
         <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
@@ -23,18 +36,30 @@ function Signup() {
                 <form action="">
                 <div className="mb-3">
                         <label htmlFor="name"><strong>Name</strong></label>
-                        <input type="text" placeholder="Enter Name" className="form-control rounded-0" />
+                        <input type="text" placeholder="Enter Name" className="form-control rounded-0" 
+                        name="name"/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email"><strong>Email</strong></label>
                         <input type="email" placeholder="Enter Email" 
-                        onChange={handleInput} className="form-control rounded-0" />
+                        onChange={handleInput} className="form-control rounded-0" 
+                        name="email"/>
                         {errors.email && <span className="text-danger"> {errors.email}</span>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="phone"><strong>Phone</strong></label>
+                        <input type="text" placeholder="Enter Phone" className="form-control rounded-0"
+                        name="phone" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="quota"><strong>Quota</strong></label>
+                        <input type="number" placeholder="Enter Quota" className="form-control rounded-0" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder="Enter Password" 
-                        onChange={handleInput} className="form-control rounded-0" />
+                        onChange={handleInput} className="form-control rounded-0"
+                        name="password" />
                         <span>{errors.password && <span> {errors.password}</span>}</span>
                     </div>
                     <button className="btn btn-success w-100 rounded-0" onSubmit={handleSubmit} ><strong>Sign up</strong></button>
